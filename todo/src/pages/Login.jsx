345bc330e3506dceb4041583e2347ecf352268ba
Poleron402 <zhperon@gmail.com>
2024-02-02 18:31:15 -0800

    started login page

--- todo/src/pages/Login.jsx
+++ todo/src/pages/Login.jsx
@@ -1,10 +1,20 @@
+import './Login.css'
+import bg from '../assets/unsplash.jpg'
 export const Login = () =>{
     return(
         <>
-            <div>
-                <h1>Welcome back</h1>
+        <div className='lisu'>
+            <div className="auth_div">
+                <div id="left">
+                    <img src={bg} id='login_bg'></img>
+                    <h1>Todo</h1>
+                </div>
+                <div id="right">
+                    <h1>Welcome back</h1>
+                </div>
+                
             </div>
-            
+        </div>
         </>
     )
 }
